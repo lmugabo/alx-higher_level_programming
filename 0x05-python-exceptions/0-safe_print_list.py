c01472bdbe0cb4504ef36d920d99887f7f0b7ea4
lmugabo <mugaboli@yahoo.com>
2021-09-29 02:49:21 -0700

    add files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-prints = 0
-for i in range(0, x):
-try:
-print(my_list[i], end='')
-prints += 1
-except IndexError:
-break
-print()
-return prints
+        count = 0
+            for i in range(0, x):
+                        try:
+                                        print("{:d}".format(my_list[i]), end="")
+                                                    count += 1
+                        except:
+                                        pass
+                                        print()
+                                            return count
+                                        
