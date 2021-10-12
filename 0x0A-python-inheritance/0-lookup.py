91701c1e60a5bd0b6ba2710f56fabd7cf2aa535b
lmugabo <mugaboli@yahoo.com>
2021-10-12 02:38:56 -0700

    what now

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Documentation for a lookup function"""
+"""A lookup function"""
 
 
 def lookup(obj):
     """Looks up the objects attributes"""
 
-return dir(obj)
+    return dir(obj)
+
