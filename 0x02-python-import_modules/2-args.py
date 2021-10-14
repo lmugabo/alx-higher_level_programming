01d30f7dceb5638757ce775d5051ba94befe131d
lmugabo <mugaboli@yahoo.com>
2021-10-14 05:39:09 -0700

    umwuka

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""
+program that prints the number of and the list of its arguments
 
+Author: Lauren Mugabo Inkingi
+"""
 if __name__ == "__main__":
-"""Print the number of and list of arguments."""
-import sys
-count = len(sys.argv) - 1
-if count == 0:
-print("0 arguments.")
-elif count == 1:
-print("1 arguments:")
-else:
-print("{:d} arguments:".format(count))
-for i in range(count):
-print("{:d}: {:s}".format(i + 1, sys.argv[i + 1])
+    """Print the number of and list of arguments."""
+    import sys
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 arguments:")
+    else:
+        print("{:d} arguments:".format(count))
+        for i in range(count):
+            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1])
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""
+Variable load
+"""
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
-from variable_load_5 import a
-print(a)
-                
+
+    from variable_load_5 import a
+    print(a)            
