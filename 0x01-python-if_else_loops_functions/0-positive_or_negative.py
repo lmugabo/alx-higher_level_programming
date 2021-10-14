fc6e3cc4276894f93dfd296ef2bef5f465666414
lmugabo <mugaboli@yahoo.com>
2021-10-13 23:29:15 -0700

    turebe

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""
+is the number positive, negative or zero
+
+Author: Lauren Mugabo Inkingi
+"""
 import random
 number = random.randint(-10, 10)
 if number > 0:
-print("{} is positive".format(number))
+    print("{} is positive".format(number))
 elif number == 0:
-print("{} is zero".format(number))
+    print("{} is zero".format(number))
 else:
-print("{} is negative".format(number))
-
+    print("{} is negative".format(number))
         
