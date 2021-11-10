e09c9867daf884a868c151066fef31a7008bfbb0
lmugabo <mugaboli@yahoo.com>
2021-11-10 01:15:35 -0800

    alo

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -58,6 +58,7 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
+
 class SinglyLinkedList:
     """Singly linked list class
     """
@@ -84,7 +85,6 @@ class SinglyLinkedList:
                 tmp = tmp.next_node
                 new.next_node = tmp.next_node
                 tmp.next_node = new
-
 
     def __str__(self):
         """Define the print() representation of a SinglyLinkedList."""
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,6 @@ class Square():
         """Square class for a quadrilateral with four equal sides"""
 
         def __init__(self, size=0):
-                
                 if not isinstance(size, int):
                         raise TypeError("size must be an integer")
                 elif size < 0:
