3e42c56ebb3ae7ecf7b90fcb514dcddeab2a3809
lmugabo <mugaboli@yahoo.com>
2021-11-18 01:32:00 -0800

    Lord have mercy

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,8 @@
--- lists all genres from hbtn_0d_tvshows and displays the number of shows in each
-SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_shows'
-FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY genre
-ORDER BY number_shows DESC;
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT g.'name' AS 'genre',
+COUNT(*) AS 'number_of_shows'
+FROM 'tv_genres' AS g
+INNER JOIN 'tv_show_gen
+ON g.'id' = t.'genre_id'
+GROUP BY g.'name'
+ORDER BY 'number_of_shows' DESC;
