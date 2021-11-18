587dcf26ce34f68cdc030690578b2bcf55021fcb
lmugabo <mugaboli@yahoo.com>
2021-11-17 23:43:44 -0800

    corrections 1.1

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 --Update score of Bob in table
-UPDATE second_table SET score=10 WHERE name='Bob';
+UPDATE second_table SET score=10 WHERE name="Bob";
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 --list the number of records with the same score in the table
-SELECT score, COUNT(score) AS 'number'FROM second_table GROUP BY score DESC;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
